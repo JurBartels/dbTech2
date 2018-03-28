92a601da600ed21029cfe69991b843dbde7772b0
rrIrene <irene.panuju@gmail.com>
2018-03-28 18:34:02 +0200

    added exhaustive index and exhaustive projection

--- include/SimpleEvaluator.h
+++ include/SimpleEvaluator.h
@@ -13,15 +13,36 @@
 #include "Evaluator.h"
 #include "Graph.h"
 
+struct aggregateIndexes {
+    // aggregate indexes: SP, SO, PS, PO, OS, OP, S, P, and O
+    std::vector<std::vector<int>> SP;
+    std::vector<std::vector<int>> SO;
+    std::vector<std::vector<int>> PS;
+    std::vector<std::vector<int>> PO;
+    std::vector<std::vector<int>> OS;
+    std::vector<std::vector<int>> OP;
+};
+
+struct exhaustiveIndexes {
+    // exhaustive indexes: SOP, PSO, POS, OSP
+    std::vector<std::vector<std::pair<uint32_t,uint32_t>>> SOP;
+    std::vector<std::vector<std::pair<uint32_t,uint32_t>>> PSO;
+    std::vector<std::vector<std::pair<uint32_t,uint32_t>>> POS;
+    std::vector<std::vector<std::pair<uint32_t,uint32_t>>> OSP;
+};
+
 class SimpleEvaluator : public Evaluator {
 
     std::shared_ptr<SimpleGraph> graph;
     std::shared_ptr<SimpleEstimator> est;
 
+    aggregateIndexes agg_indexes;
+    exhaustiveIndexes exh_indexes;
+
 public:
 
     explicit SimpleEvaluator(std::shared_ptr<SimpleGraph> &g);
-    ~SimpleEvaluator() = default;
+    SimpleEvaluator() = default;
 
     void prepare() override ;
     cardStat evaluate(RPQTree *query) override ;
@@ -33,6 +54,10 @@ public:
     static std::shared_ptr<SimpleGraph> join(std::shared_ptr<SimpleGraph> &left, std::shared_ptr<SimpleGraph> &right);
 
     static cardStat computeStats(std::shared_ptr<SimpleGraph> &g);
+    void createAggregateIndex();
+    void createExhaustiveIndex();
+    std::shared_ptr<SimpleGraph> project_agg_index(uint32_t label, bool inverse, std::shared_ptr<SimpleGraph> &g);
+    std::shared_ptr<SimpleGraph> project_exh_index(uint32_t label, bool inverse, std::shared_ptr<SimpleGraph> &g);
 
 };
 
--- src/SimpleEvaluator.cpp
+++ src/SimpleEvaluator.cpp
@@ -22,6 +22,50 @@ void SimpleEvaluator::prepare() {
 
     // prepare other things here.., if necessary
 
+    //createAggregateIndex();
+    createExhaustiveIndex();
+}
+
+void SimpleEvaluator::createExhaustiveIndex() {
+    // exhaustive indexes: SOP, PSO, POS, OSP
+    exh_indexes.POS.resize(graph->getNoLabels());
+    exh_indexes.PSO.resize(graph->getNoLabels());
+    for(uint32_t j = 0; j < graph->getNoVertices(); j++) {
+        for (auto edge: graph->adj[j]) {
+            //edge.first = edge type, edge.second = out node, j = in node
+            // POS = edge type -> (out node, in node)
+            exh_indexes.POS[edge.first].push_back(std::make_pair(edge.second, j));
+            // PSO = edge type -> (in node, out node)
+            exh_indexes.PSO[edge.first].push_back(std::make_pair(j, edge.second));
+        }
+    }
+}
+
+void SimpleEvaluator::createAggregateIndex() {
+    // aggregate indexes: SP, SO, PS, PO, OS, OP, S, P, and O
+    agg_indexes.SP.resize(graph->getNoVertices());
+    agg_indexes.SO.resize(graph->getNoVertices());
+    agg_indexes.PS.resize(graph->getNoLabels());
+    agg_indexes.PO.resize(graph->getNoLabels());
+    agg_indexes.OS.resize(graph->getNoVertices());
+    agg_indexes.OP.resize(graph->getNoVertices());
+    for(uint32_t j = 0; j < graph->getNoVertices(); j++) {
+        for (auto edge: graph->adj[j]) {
+            //edge.first = edge type, edge.second = out node, j = in node
+            // SP = in node -> edge type
+            agg_indexes.SP[j].push_back(edge.first);
+            // SO = in node -> out node
+            agg_indexes.SO[j].push_back(edge.second);
+            // PS = edge type -> in node
+            agg_indexes.PS[edge.first].push_back(j);
+            // PO = edge type -> out node
+            agg_indexes.PO[edge.first].push_back(edge.second);
+            // OS = out node -> in node
+            agg_indexes.OS[edge.second].push_back(j);
+            // OP = out node + edge type
+            agg_indexes.OP[edge.second].push_back(edge.first);
+        }
+    }
 }
 
 cardStat SimpleEvaluator::computeStats(std::shared_ptr<SimpleGraph> &g) {
@@ -68,6 +112,58 @@ std::shared_ptr<SimpleGraph> SimpleEvaluator::project(uint32_t projectLabel, boo
 
                 if (label == projectLabel)
                     out->addEdge(source, target, label);
+            }
+        }
+    }
+
+    return out;
+}
+
+std::shared_ptr<SimpleGraph> SimpleEvaluator::project_exh_index(uint32_t projectLabel, bool inverse, std::shared_ptr<SimpleGraph> &in) {
+    auto out = std::make_shared<SimpleGraph>(in->getNoVertices());
+    out->setNoLabels(in->getNoLabels());
+
+    auto PSO = exh_indexes.PSO[projectLabel];
+    auto POS = exh_indexes.POS[projectLabel];
+
+    if (!inverse) {
+        // forward
+        for (auto edge : PSO) {
+            // edge.first = in node, edge.second = out node
+            out->addEdge(edge.first, edge.second, projectLabel);
+        }
+    } else {
+        // backward
+        for (auto edge : POS) {
+            // edge.first = out node, edge.second = in node
+            out->addEdge(edge.first, edge.second, projectLabel);
+        }
+    }
+
+    return out;
+}
+
+std::shared_ptr<SimpleGraph> SimpleEvaluator::project_agg_index(uint32_t projectLabel, bool inverse, std::shared_ptr<SimpleGraph> &in) {
+
+    // find all nodes connected to edge type projectLabel
+    std::vector<int> PS = agg_indexes.PS[projectLabel];
+    std::vector<int> PO = agg_indexes.PO[projectLabel];
+
+    auto out = std::make_shared<SimpleGraph>(in->getNoVertices());
+    out->setNoLabels(in->getNoLabels());
+
+    if (!inverse) {
+        // forward
+        for (auto inNode : PS) {
+            for (auto outNode: PO) {
+                out->addEdge(inNode, outNode, projectLabel);
+            }
+        }
+    } else {
+        // backward
+        for (auto outNode : PO) {
+            for (auto inNode: PS) {
+                out->addEdge(inNode, outNode, projectLabel);
             }
         }
     }
@@ -122,7 +218,9 @@ std::shared_ptr<SimpleGraph> SimpleEvaluator::evaluate_aux(RPQTree *q) {
             return nullptr;
         }
 
-        return SimpleEvaluator::project(label, inverse, graph);
+        //return SimpleEvaluator::project(label, inverse, graph);
+        //return SimpleEvaluator::project_agg_index(label, inverse, graph);
+        return SimpleEvaluator::project_exh_index(label, inverse, graph);
     }
 
     if(q->isConcat()) {
